a3492b2ddd9b36f7b103eb4da5ecd00342099e29
Shubham Meshram <shubham9325@gmail.com>
2021-12-19 11:29:22 +0530

    Chahawalebhau

---
+++ include/footer.php
@@ -0,0 +1,11 @@
+
+<div class="card text-right">
+    <div class="card-header ">
+      contact Us
+    </div>
+    <div class="card-body">
+      <h5 class="card-title">Started By</h5>
+      <p class="">Vishal Bramhakar & Pawan Gabane</p>
+      <p class="">Contact No. +91 7719862696</p>
+    </div>
+  </div>
---
+++ index.php
@@ -0,0 +1,137 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+  <!-- Google font -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Crimson+Pro">
+
+  <title>Chahawalebhau.com</title>
+  <style>
+    * {
+      padding :0;
+      margin :0;
+    }
+    body {
+        font-family: 'Crimson Pro', serif;
+        font-size: 20px;
+      }
+    .navbar {
+      background: red;
+    }
+    p {
+      margin-bottom : 0;
+    }
+  </style>
+</head>
+
+<body>
+
+
+
+
+  <div class="container-fluid">
+
+    <!-- Navbar start -->
+
+    <nav class="navbar navbar-expand-lg navbar-light ">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03"
+        aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <a class="navbar-brand" href="#">चहावाले भाऊ</a>
+
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Namskar</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About Us</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="#">Our Franchise</a>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <i class="fa fa-camera" aria-hidden="true"></i>
+        </form>
+      </div>
+    </nav>
+    <!-- Navbar End -->
+
+
+
+    <div id="carouselExampleControls" class="carousel slide mt-2" data-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img class="d-block w-100" src="30563.jpg" alt="First slide">
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" src="30563.jpg" alt="Second slide">
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" src="30563.jpg" alt="Third slide">
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+    <div class="container-fluid">
+      <div class="row border text-center">
+          <div class="col-md-12">
+            We Offer
+          </div>
+        </div>
+        <div class="row text-center">
+          <div class="col-md-6 border">
+            Special Tea
+          </div>
+          <div class="col-sm-6 border">
+            50 Rs.
+          </div>
+        </div>
+      </div>
+
+
+    <div>
+      <?php include('include/footer.php'); ?>
+    </div>
+
+
+    </div>
+
+
+    
+
+
+  </div>
+
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+    crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+    integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+    crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+    integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
